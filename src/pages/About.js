7684472113d67f28a31da68247e30cc2c9d641dd
Manish Raj <42971673+manishrajmishra@users.noreply.github.com>
2020-12-23 15:54:04 +0530

    updated about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+        TheCocktailDB is built in 2020 to provide a free data source for drinks.
+        A cocktail is an alcoholic mixed drink, which is either a combination of
+        spirits, or one or more spirits mixed with other ingredients such as
+        fruit juice, flavored syrup, or cream. There are various types of
+        cocktails, based on the number and kind of ingredients added. The
+        origins of the word cocktail have been debated. It is developed and
+        maintained by Manish Raj
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
